23d7ec7056e8a8884722db39b19617ef82314bd0
saurabh sharma <saurabh.sharma232@outlook.com>
2025-03-24 20:10:27 +0530

    Add initial implementation of book management features with context API, including create, edit, delete, and list functionalities

---
+++ bookswithcontext/src/components/BookList.js
@@ -0,0 +1,15 @@
+import { useContext } from 'react';
+import BookShow from './BookShow';
+import BooksContext from '../context/books';
+function BookList() {
+    const { books } = useContext(BooksContext);
+
+    const renderedBooks = books.map((book) => {
+        return <BookShow key={book.id} book={book} />
+    });
+    return <div className="book-list">
+        {renderedBooks}
+    </div>
+}
+
+export default BookList;
